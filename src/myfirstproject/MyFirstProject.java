7db3246dd2bdd50ca31d1df5f55da601256623f3
pupil <pupil@ivkhk.ee>
2023-09-14 12:55:08 +0300

    first com

---
+++ src/myfirstproject/MyFirstProject.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstproject;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class MyFirstProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String firstname = scanner.nextLine();
+        System.out.print("Type ypur second name:");
+        String lastname = scanner.nextLine();
+        System.out.println("Hello JPTV22" + " " + firstname + " " + lastname);
+        scanner.close();*/
+        int k =  10;
+        System.out.println(k);
+    }
+    
+}
